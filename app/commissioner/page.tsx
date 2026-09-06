0e53d7f876608c83a5fea350a0e4f816146345b8
donlovesjoy-ai <donlovesjoy@gmail.com>
2026-09-06 17:52:34 -0400

    Rename commissioner hub

--- app/commissioner/page.tsx
+++ app/commissioner/page.tsx
@@ -66,7 +66,7 @@ export default async function CommissionerHub(){
           </div>
 
           <div className="muted">
-            Commissioner Ship
+            Commissioner Shit
           </div>
         </div>
       </div>
@@ -80,7 +80,7 @@ export default async function CommissionerHub(){
         }}
       >
         <h1>
-          Commissioner Ship
+          Commissioner Shit
         </h1>
 
         <div
